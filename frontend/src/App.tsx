490ec934e05afb0850a2505c73b7093ccf2853b3
Wojciech Kaspera <wojciech.kaspera@gmail.com>
2020-11-27 20:02:55 +0100

    material UI added to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Wrapper from "./components/web-wrapper/web-wrapper";
+import Button from "@material-ui/core/Button";
 
 function App() {
-  return (
-    <Wrapper app={(() => {
-      return (
-          <div>Zajebiście</div>
-      )
-    })()}></Wrapper>
-  );
+    return (
+        <Wrapper app={(() => {
+            return (
+                <>
+                    <div>Fajnie fest</div>
+                    <Button variant='contained' color='primary'>Spoko button</Button>
+                </>
+            )
+        })()}></Wrapper>
+    );
 }
 
 export default App;
