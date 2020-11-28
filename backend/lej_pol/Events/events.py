364b44bb60333f5702e3be6cdcb25f30c6b8c5cf
Leszek Malec <raven9437@gmail.com>
2020-11-28 15:03:19 +0100

    format changed

--- backend/lej_pol/Events/events.py
+++ backend/lej_pol/Events/events.py
@@ -1,5 +1,6 @@
 import json
 import os
+import requests
 
 from flask_socketio import emit, send
 
@@ -7,18 +8,22 @@ from lej_pol import socketio
 from rx import from_
 from rx import operators as ops
 
+
+ml_endpoint = "http://127.0.0.1.8000/model_ml"
+
 source = from_(open(os.path.join(".", "test_flow.dat")))
 
 source.pipe(
     ops.map(lambda row: json.loads(row)),
     # ops.map(lambda row: {row["time"]: round(row["flow"], 5)}),
-    ops.buffer_with_count(3),
+    ops.buffer_with_count(10),
     ops.map(lambda row: sorted(row, key=lambda k: int(k["time"]))),
-    ops.map(lambda row: {el["time"]: round(el["flow"], 5) for el in row}),
+    ops.map(lambda row: {str(idx): [round(el["flow"], 5)] for idx, el in enumerate(row)}),
     # ops.map(lambda row: {"data": {k: v for d in row for k, v in d.items()}}),
     ops.map(lambda row: {"data": row}),
     ops.map(lambda row: json.dumps(row))
 ).subscribe(on_next=lambda row: print(row))
+# .subscribe(on_next=lambda row: requests.post(ml_endpoint, row))
 
 
 @socketio.on('message')
@@ -31,6 +36,5 @@ def handle_connection():
     emit('ok', {'connected': True}, broadcast=True)
 
 
-@socketio.on('json')
-def handle_json(json_data):
-    emit(json_data, json=True, broadcast=True, namespace="/model_ml")
+
+
