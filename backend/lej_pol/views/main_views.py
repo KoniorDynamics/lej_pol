57ea8baeed65e9b9402c615f46abc4f2c9cc224e
Katarzyna Rzesikowska <katarzyna.rzesikowska@gmail.com>
2020-11-29 08:15:53 +0100

    change data flow

--- backend/lej_pol/views/main_views.py
+++ backend/lej_pol/views/main_views.py
@@ -13,11 +13,15 @@ def post_flow():
     global DATA_LIST
     DATA_LIST.append(data)
     if len(DATA_LIST) == 10:
+        test = []
         data_to_analyze = {}
         for idx, el in enumerate(DATA_LIST):
             data_to_analyze[idx] = [round(el["flow"], 5)]
-        print(data_to_analyze)
-        send_notification({"data": data_to_analyze})
-        DATA_LIST = []
+            test.append(round(el["flow"], 5))
+        if len(set(test)) == 1 and test[0] == 0:
+            DATA_LIST = []
+        else:
+            send_notification({"data": data_to_analyze})
+            DATA_LIST = []
 
-    return 'test'
+    return 'Flow'
