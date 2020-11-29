647089a4ab1648e2868594794cbba8d8b9d9bc08
Wojciech Kaspera <wojciech.kaspera@gmail.com>
2020-11-29 09:43:18 +0100

    url update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -73,7 +73,7 @@ function App() {
 
     const fetchNotifications = () => {
         setSpinnerState(true);
-        axios.get('http://127.0.0.1:5000/notification')
+        axios.get('http://35.246.205.45:5000/notification')
             .then(
                 response => {
                     setNotifications(response.data);
--- frontend_licznik/src/websocket.js
+++ frontend_licznik/src/websocket.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient('http://127.0.0.1:5000/api_v1/water_meter');
+const socket = socketIOClient('http://35.246.205.45:5000/api_v1/water_meter');
 
 const initiateWebsocketConnection = () => {
     socket.on('connected', () => {
